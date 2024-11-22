e3df5acf4ee5e2f9212fced40e0637cb071d5a50
Connor Dailey <connorldailey@gmail.com>
2024-11-22 10:58:40 -0500

    Add authentication to API routes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,35 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  // Extract username and password from the request body
+  const { username, password } = req.body;
+
+  // Find the user in the database by username
+  const user = await User.findOne({
+    where: { username },
+  });
+
+  // If the user is not found, send an authentication failed response
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  // Compare the provided password with the stored hashed password
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+
+  // If the password is invalid, send an authentication failed response
+  if (!passwordIsValid) {
+    res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  // Get the secret key from the environment vairables
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  // Generate a JWT token for the authentiated user
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+  // Send the token as a JSON response
+  return res.json({ token });
 };
 
 const router = Router();
