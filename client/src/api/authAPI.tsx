d640613f4d387f66076b3d5289e39fc647552384
Connor Dailey <connorldailey@gmail.com>
2024-11-22 11:20:30 -0500

    Complete the client side login method

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,35 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
+// Function to send a POST request to the '/auth/login' endpoint with user login information
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+  try {
+    // Send a POST request to '/auth/login' with user login information in JSON format
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    // Throw error if response status is not OK
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}`);
+    }
+    
+    // Parse the response body as JSON
+    const data = await response.json();
+
+    // Return the data received from the server
+    return data;
+  } catch (err) {
+    // Log any errors that occur during fetch
+    console.log('Error from user login:', err);
+
+    // Return a rejected promise with an error message
+    return Promise.reject('Could not fetch user info');
+  }
 }
-
-
 
 export { login };
